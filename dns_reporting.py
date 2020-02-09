f086600f9cd404d3d1e1454126d258a2cc540ba3
Paul Mahon <paulmahon@me.com>
2020-02-08 20:56:47 -0500

    printing update

--- dns_reporting.py
+++ dns_reporting.py
@@ -8,13 +8,28 @@ from socket import gethostbyaddr
 
 start = (time.time())  # track the execution of this module
 
+
+# used for various printing - this is a generator to print the first N lines
+def printing_station(dic_object, count):
+    counter = 0
+    for k, v in dic_object.items():
+        if counter == count:
+            return
+        counter += 1
+        yield "{:<50} {}".format(k, v)
+
+
 def get_domain_list():
     search_pattern = re.compile(r'query:\s(.*?)\s')
-    domain_list = re.findall(search_pattern,query_log)
+    domain_list = re.findall(search_pattern, query_log)
     temp = Counter(domain_list)
-    top_list = sorted(temp.items(), key=itemgetter(1), reverse=True)
-    for i in range(0,20):
-        print("{:<40} {}".format(top_list[i][0],top_list[i][1]))
+    top_list = dict(sorted(temp.items(), key=itemgetter(1), reverse=True))  # Sort the list and return a dictionary
+    wl = white_list.split('\n')
+    [top_list.pop(y) for x in wl for y in top_list.copy() if re.search(x, y)]  # filter out certain safe domains
+
+    for items in printing_station(top_list, 20):  # call the printing generator
+        print(items)
+
 
 # function to parse the dhcp lease file for host names if not resolved by getaddr
 def get_name_dhcp(ip):
@@ -41,9 +56,11 @@ def open_file(filename):
         ret_object = of.read()
         return ret_object
 
+
 # Open the files for processing
 dhcpleases = open_file('/var/lib/dhcp/dhcpd.leases')
 query_log = open_file('/var/cache/bind/querylog')
+white_list = open_file('white_list.txt')
 
 
 def get_ip_activity():
@@ -52,19 +69,17 @@ def get_ip_activity():
     query_list = re.findall(p1, query_log)
     # run the counter ot get instances of IPs domain request.
     query_dic = Counter(query_list)  # make a dict with the ip as keys and count of each request as the value
-    finallist = sorted(query_dic.items(), key=itemgetter(1), reverse=True)  # provides a sorted list
-
+    finallist = dict(sorted(query_dic.items(), key=itemgetter(1), reverse=True))  # provides a sorted list
 
     print("Top 20 List \t \t \t  Hits")
-    for i in range(0, 20):
-        if finallist[i]:
-            print("IP:{} \t {:>10} \t {:>10}".format(finallist[i][0], finallist[i][1], get_name_dns(finallist[i][0][1:])))
-
-    finish = time.time()
-    print("\nTime to run this program:  {}\n".format(finish - start))
+    for items in printing_station(finallist, 20):
+        print("{} \t {}".format(items, get_name_dns(items.split(" ")[1])))
+    print("\n")
 
 
 if __name__ == '__main__':
     get_ip_activity()
     get_domain_list()
 
+finish = time.time()
+print("\nTime to execute:  {}\n".format(finish - start))
