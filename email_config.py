38a8557b22372353b67fdcfcaa282c5ad7310276
Paul Mahon <paulmahon@me.com>
2020-02-24 17:54:57 -0500

    class update

--- email_config.py
+++ email_config.py
@@ -1,20 +1,22 @@
+import smtplib
+
+
 class Emailconfig():
 
-# should not be replicated
+    def __init__(self):
+        self.fromaddr = 'username@gmail.com'  # your from address @gmail.com
+        self.toaddr = 'anyaddress@anyname.com'  # to address
+        self.email_pass = 'enterpassword'  # password
+        self.elogin = 'username'  # your login ID typically w/o @gmail.com
 
     def send_email(self, btext):
-        import smtplib
 
         # update your settings for your google mail (gmail)  account here
-        fromaddr = 'username@gmail.com'  # your from address @gmail.com
-        toaddr = 'anyaddress@anyname.com'  # to address
-        email_pass = 'enterpassword'  # password
-        elogin = 'username'  # your login ID typically w/o @gmail.com
 
         try:
             server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.login(elogin, email_pass)
-            server.sendmail(fromaddr, toaddr, btext)
+            server.login(self.elogin, self.email_pass)
+            server.sendmail(self.fromaddr, self.toaddr, btext)
             server.close()
             print("email sent successfully")
         except:
